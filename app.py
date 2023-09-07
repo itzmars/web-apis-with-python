f9c2b09d36e86421997b2ef2321e40bd427dc60f
Owens003 <owensmathurin03@gmail.com>
2023-09-07 09:25:06 -0400

    google api  initerface

--- app.py
+++ app.py
@@ -12,22 +12,38 @@ def index():
     This method will
     1. Provide usage instructions formatted as JSON
     """
-    # response = {"usage": "/dict?=<word>"}
+    response = {"usage": "/dict?=<word>"}
     # Since this is a website with front-end, we don't need to send the usage instructions
-    return "TODO"
+    return render_template("index.html")
 
 
 @app.get("/dict")
 def dictionary():
-    """
-    DEFAULT ROUTE
-    This method will
-    1. Accept a word from the request
-    2. Try to find an exact match, and return it if found
-    3. If not found, find all approximate matches and return
-    """
-    return "TODO"
+    words = request.args.getlist("word")
 
+    if not words:
+        
+        response = ({"status":"error","word":"words", "data": "word not fount"})
+        return jsonify(response)
+    
+    response = {"words":[]}
+
+    for word in words:
+    
+        definitions =  match_exact(word)
+
+        if definitions:
+            response["words"].append({"status":"success", "data":definitions, "word":word})
+        else:    
+        
+            definitions = match_like(word)
+
+            if definitions:
+                response["words"].append({"status":"partial", "data":definitions,"word":word})
+            else:
+                response["words"].append({"status":"error", "data":"word not found", "word":word})
+
+    return render_template("results.html", response=jsonify(response))
 
 if __name__ == "__main__":
     app.run()
